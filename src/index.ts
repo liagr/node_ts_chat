71c4cfe47732019e01d82bbb2e183d7afeaea076
leagr <guindeya@gmail.com>
2018-02-08 14:11:53 +0300

    init env configurations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('dafads');
+import * as dotenv from 'dotenv'
+import * as path from 'path'
+
+const dev = dotenv.config({ path: path.resolve(process.cwd(), '.development.env') }).parsed
+const prod = dotenv.config({ path: path.resolve(process.cwd(), '.production.env') }).parsed
